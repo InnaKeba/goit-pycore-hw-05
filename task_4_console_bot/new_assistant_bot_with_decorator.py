2269ec1e27651d93ba2ab1908125d44236abf3cb
InnaKeba <kebainna@gmail.com>
2025-06-28 17:59:49 +0300

    Add error handling to console bot using input_error decorator

---
+++ task_4_console_bot/new_assistant_bot_with_decorator.py
@@ -0,0 +1,83 @@
+"""Доробіть консольного бота помічника з попереднього домашнього завдання та додайте обробку помилок за допомоги декораторів."""
+
+def input_error(func):
+    """Декоратор для обробки помилок введення користувача."""
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return "Contact not found."
+        except ValueError:
+            return "Give me name and phone please."
+        except IndexError:
+            return "Enter user name."
+    return wrapper
+
+def parse_input(user_input):
+    """Парсить введену користувачем команду та аргументи."""
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+@input_error
+def add_contact(args, contacts):
+    """Додає новий контакт до списку контактів."""
+    if len(args) != 2:
+        raise ValueError
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+@input_error
+def change_contact(args, contacts):
+    """Змінює номер телефону для існуючого контакту."""
+    if len(args) != 2:
+        raise ValueError
+    name, new_phone = args
+    if name not in contacts:
+        raise KeyError
+    contacts[name] = new_phone
+    return "Contact updated."
+
+@input_error
+def show_phone(args, contacts):
+    """Виводить номер телефону для вказаного контакту."""
+    if len(args) != 1:
+        raise IndexError
+    name = args[0]
+    if name not in contacts:
+        raise KeyError
+    return contacts[name]
+
+def show_all(contacts):
+    """Виводить усі збережені контакти з номерами телефонів."""
+    if not contacts:
+        return "No contacts saved."
+    return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
+
+def main():
+    """Головна функція для запуску асистент-бота."""
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            print(show_all(contacts))
+        else:
+            print("Invalid command.")
+if __name__ == "__main__":
+    main()
+
+#Запуск бота - python new_assistant_bot_with_decorator.py
